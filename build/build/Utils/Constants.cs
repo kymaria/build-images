15f6266524db01fad9fff670ebfed43b611cc38d
Jon Stelly <967068+jonstelly@users.noreply.github.com>
2021-07-06 23:06:42 -0500

    Switch docker repository to publish samples
    Signed-off-by: Jon Stelly <967068+jonstelly@users.noreply.github.com>

--- build/build/Utils/Constants.cs
+++ build/build/Utils/Constants.cs
@@ -3,5 +3,5 @@ public class Constants
     public const string ToolsDirectory = "./tools";
     public static readonly string DockerHubRegistry = $"docker.io/{DockerImageName}";
     public static readonly string GitHubContainerRegistry = $"ghcr.io/{DockerImageName}";
-    private const string DockerImageName = "gittools/build-images";
+    private const string DockerImageName = "kymaria/gt-build-images";
 }
